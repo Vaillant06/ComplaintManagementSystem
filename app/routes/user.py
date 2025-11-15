32ef92a8193d26d29b70d9a3b544c283ea2cf920
SREENATH G <sreenath2410530@ssn.edu.in>
2025-11-15 21:23:40 +0530

    Added filter functionality and improved user profile UI/UX

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, flash, request
 from flask_login import login_required, current_user
+from datetime import timedelta
 from app.db import query
 
 bp = Blueprint("user", __name__, url_prefix="/user")
@@ -7,11 +8,46 @@ bp = Blueprint("user", __name__, url_prefix="/user")
 @bp.route("/")
 @login_required
 def user_dashboard():
-    rows = query(
-        "select * from complaints where user_id=%s order by created_at desc",
+    user = query(
+        "SELECT * FROM users WHERE id=%s",
         (current_user.id,),
-        fetchall = True
+        fetchone=True
     )
+
+    status = request.args.get("status")
+
+    if status:
+        rows = query(
+            """
+            SELECT c.*, u.name AS user_name, d.name AS department_name
+            FROM complaints c
+            JOIN users u ON c.user_id = u.id
+            JOIN departments d ON d.id = c.department_id
+            WHERE c.status = %s
+            ORDER BY c.created_at DESC
+            """,
+            (status,),
+            fetchall=True
+        )
+
+    else:
+        rows = query(
+            """
+            SELECT c.*, u.name AS user_name, d.name AS department_name
+            FROM complaints c
+            JOIN users u ON c.user_id = u.id
+            JOIN departments d ON d.id = c.department_id
+            ORDER BY c.created_at DESC
+            """,
+            fetchall=True
+        )
+
+    for c in rows:
+        c["created_at"] = (c["created_at"] + timedelta(hours=5, minutes=30))\
+                            .strftime("%H:%M | %d-%m-%Y")
+
+    if user["last_login"]:
+        user["last_login"] = (user["last_login"] + timedelta(hours=5, minutes=30))\
+                        .strftime("%H:%M  |  %d-%m-%Y")
     
-    flash("Login Successful", "success")
-    return render_template("user_dashboard.html", complaints=rows )
+    return render_template("user_dashboard.html", complaints=rows, user=user, status=status)
